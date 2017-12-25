8827b58c6aa81ae291b8defdd3202ab2fbcaf42b
Veronica Kim <vero.a.kim@gmail.com>
2017-12-25 21:56:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,25 @@
 # Your code goes here!
 class Dog
-  def initialize(name, bark)
-    @name = name
-    @bark = bark
-  end
-
-  def name=(name)
-    @name = nameE
-  end
-
-  def name
-    @name
-  end
-
-  def bark=(bark)
-    @bark = bark
-      puts "woof!"
-  end
-
-  def bark
-    @bark
-  end
+  # def initialize(name, bark)
+  #   @name = name
+  #   @bark = bark
+  # end
+  # 
+  # def name=(name)
+  #   @name = nameE
+  # end
+  # 
+  # def name
+  #   @name
+  # end
+  # 
+  # def bark=(bark)
+  #   @bark = bark
+  #     puts "woof!"
+  # end
+  # 
+  # def bark
+  #   @bark
+  # end
 
 end
